29266e967b0016bd40c46779c10a0da8a55d7096
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-10-08 21:07:48 +0200

    nigdy wiecej infy na iecie for real,cw 1 zr w 90%

--- cwiczenia2020/cw1.py
+++ cwiczenia2020/cw1.py
@@ -1,4 +1,6 @@
 #Cw1 - proste programy z pętlami cz 1 ~~lpawlak
+from math import sqrt
+
 def cw1():
     a1 = 1
     a2 = 1
@@ -30,13 +32,13 @@ def cw3(searched_number):
     a1 : int = 1
     a2 : int = 1
     suma : int = 0
+    b1 = 1
+    b2 = 1
     while True:
         suma += a1
         if searched_number - suma == 0:
             return True
         if searched_number < suma:
-            b1 = 1
-            b2 = 1
             while True:
                 if suma - b1 == searched_number:
                     return True
@@ -59,7 +61,7 @@ def cw4(searched_number):
         if suma == searched_number:
             return ret 
         if suma > searched_number:
-            return "Nie ma"
+            return ret-1
         ret += 1
 
 def cw5(pole):
@@ -132,6 +134,7 @@ def cw11():
         if suma_b == a and suma_a != a:
             print(a, suma_a, "Są dobre ...") 
     return "zakończono"
+
 def cw12(a =60,b=30,c=105):
     return min(nwd(a,b),nwd(a,c),nwd(b,c))
 
@@ -153,6 +156,53 @@ def cw13():
 def cw14():
     # tak szczerze to co to za typ...
     pass
-
+def cw15():
+    #for real?
+    pass
+def cw16():
+    #not working
+    maxymalna_wartosc:int = 0
+    liczba = 0
+    for i in range(2,10000+1):
+        count = 0
+        a_n = i
+        while a_n - 1.0 > 0.000000054:
+            a_n = cw16_rek(a_n)
+            count += 1
+        if count > maxymalna_wartosc:
+            maxymalna_wartosc = count
+            liczba = i
+    return maxymalna_wartosc,liczba
+def cw16_rek(a_n):
+    return (a_n%2)*(3*a_n + 1) + (1-a_n%2) *(a_n/2)
+def cw17(a = 1, b=1):
+    #hard to tell czy dziala
+    a1,a2 = a,b
+    iloraz_1, iloraz_2 = 1.0,10101010101010101010.0
+    while abs(iloraz_1 - iloraz_2) > 0.00000000001:
+        a1,a2 = a2,a1+a2
+        iloraz_1, iloraz_2 = iloraz_2,a2/a1
+    return iloraz_2
+def cw18():
+    # patrz cw 5, (spoiler alert!)nie ma go
+    pass
+def cw19():
+    ret = 1.0
+    for i in range(1,1000+1):
+        print(i)
+        ret += 1/silnia(i)
+    return ret
+def silnia(nr):
+    ret = 1
+    for i in range(1,nr+1):
+        ret *= i
+    return ret
+def cw20():
+    a_n = 20.0
+    b_n = 30.0
+    a_n,b_n = sqrt(a_n*b_n),(a_n+b_n)/2.0
+    while a_n - b_n >  0.000000000001:
+        a_n,b_n = sqrt(a_n*b_n),(a_n+b_n)/2.0
+    return b_n
 if __name__ == "__main__":
-    print(cw13())
+    print(cw17())
