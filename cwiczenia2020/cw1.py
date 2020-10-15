8cb5246be7f7e15668d86c412cebcaa9368cdb69
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-10-15 14:36:55 +0200

    asd

--- cwiczenia2020/cw1.py
+++ cwiczenia2020/cw1.py
@@ -129,18 +129,14 @@ def cw10():
 def cw11():
     maximum = 10**4
     for a in range(2,maximum+1):
-        suma_a = 0
-        for i in range(int(sqrt(a)),0,-1):
+        suma_a = 1
+        for i in range(int(sqrt(a)),1,-1):
             if a % i == 0:
-                if not(i == 1 or i == a):
-                    suma_a += a//i
-                suma_a += i
-        suma_b = 0
-        for i in range(int(sqrt(suma_a)),0,-1):
+                suma_a += (a//i) + i
+        suma_b = 1
+        for i in range(int(sqrt(suma_a)),1,-1):
             if suma_a % i == 0:
-                if not(i == 1 or i == suma_a):
-                    suma_b += suma_a//i
-                suma_b += i
+                suma_b += (suma_a//i) + i
         if suma_b == a and suma_a < a:
             print(a, suma_a, "Są dobre ...")
 
