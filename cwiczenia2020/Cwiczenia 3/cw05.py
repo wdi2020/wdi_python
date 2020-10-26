a35eac5f5e237ba7d75a45bebd6bd968050bae15
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-10-26 11:25:23 +0100

    cw5

---
+++ cwiczenia2020/Cwiczenia 3/cw05.py
@@ -0,0 +1,30 @@
+temp = int(input("10 wartosc co do wartosci wartiscisdafn, zakoncz zerem"))
+t = [0 for _ in range(100)]
+k = 0
+while temp != 0:
+    t[k] = temp
+    temp = int(input("10 wartosc co do wartosci wartiscisdafn, zakoncz zerem"))
+    k+=1
+#end while
+
+t_10 = [0 for _ in range(10)]
+i = 0
+while i < k:
+    if t[i] > t_10[9]:
+        for j in range(k):
+            if t[i] > t_10[j]:
+                #alter all and insert in
+                m = j
+                temp = t_10[m]
+                t_10[m] = t[i]
+                while m < 10-1:
+                    temp,t_10[m+1] = t_10[m+1],temp
+                    m+=1
+                break
+    i+=1
+print(t_10[9])
+            
+            
+
+
+
