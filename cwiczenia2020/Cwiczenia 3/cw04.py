2c02e7f8ca8fc4c4ef5b7dacee3f4336578f1c2f
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-10-28 11:34:15 +0100

    al

---
+++ cwiczenia2020/Cwiczenia 3/cw04.py
@@ -0,0 +1,26 @@
+#author - Nikodem K.
+N = int(input("podaj liczbe: "))
+e = 1
+a = 1
+sil = 1
+t = [0 for i in range(10000)]
+t[0] = 1
+kon = 0
+while kon == 0:
+    sil *= a
+    n = 1
+    licz = 0
+    for x in range(N+3):
+        if (n//sil) % 10 == 0:
+            licz += 1
+        if licz - 3 == N:
+            kon = 1
+        t[x] += (n//sil) % 10
+        n *= 10
+        if t[x] > 9:
+            t[x] %= 10
+            t[x-1] += 1
+    a += 1
+print(t[0], end='.')
+for x in range(1, N+1):
+    print(t[x], end="")
