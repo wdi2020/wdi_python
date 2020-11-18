d9bea2506be9c1cc1e42bfe727a1ab9cba361018
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-11-18 10:22:27 +0100

    Update cw05.py

--- cwiczenia2020/Cwiczenia 5/cw05.py
+++ cwiczenia2020/Cwiczenia 5/cw05.py
@@ -1,22 +1,19 @@
 def cw05(dane):
-#!    brudfors
+#!    brudfors powinno dzialac ale hard to tell czy tak jest pzdr
     for i in dane:
         for e in dane:
-            if i[1] == e[1] and i!=e:
-                dlugosc = abs(i[0]-e[0])
+            #tylko w prawo
+            if i[1] == e[1] and i[0] < e[0]:
+                dlugosc = e[0]-i[0]
+                #tylko w gore
                 poziomA = i[1] + dlugosc
-                poziomB = i[1] - dlugosc
                 countA = 0
-                countB = 0
                 for f in dane:
-                    if f[1] == poziomA:
+                    if ( f[0] > i[0] and f[0] < e[0] ) and ( f[1] > i[1] and f[1] < poziomA):
+                        break
+                    elif f[1] == poziomA:
                         if f[0] == i[0] or f[0] == e[0]:
                             countA+=1
                             if countA == 2:
-                                return True
-                    if f[1] == poziomB:
-                        if f[0] == i[0] or f[0] == e[0]:
-                            countB+=1
-                            if countB == 2:
                                 return True
     return False
