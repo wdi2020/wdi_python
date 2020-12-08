f9e1d57fffecbfa09d8b70b27b35b4c8c541cddd
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-12-08 21:14:07 +0100

    update 2 files lkj

--- cwiczenia2020/Cwiczenia 6/cw21.py
+++ cwiczenia2020/Cwiczenia 6/cw21.py
@@ -1,27 +1,28 @@
-def can_take(tab,wybrane,index):
-    for i in wybrane:
-        if i//len(tab) == index//len(tab) or i%len(tab) == index%len(tab):
-            return False
-    return True
+def can_take(tab,index,tablica):
+    if tablica[index] == False:
+        return True
+    return False
 
-def reku(tab,suma_curr,suma_zad,wybrane,index):
+def reku(tab,suma_curr,suma_zad,index,tablica):
     if suma_curr == suma_zad:
         return True
     elif suma_curr > suma_zad:
         return False
     elif index == (len(tab)**2) -1:
         return False
-    if reku(tab,suma_curr,suma_zad,wybrane,index+1):
+    if reku(tab,suma_curr,suma_zad,index+1,tablica):
         return True
-    if can_take(tab,wybrane,index):
-        wybrane.append(index)
-        if reku(tab,suma_curr+tab[index//len(tab)][index%len(tab)],suma_zad,wybrane,(index//len(tab))+1):
+    if can_take(tab,index,tablica):
+        tablica[index] = True
+        if reku(tab,suma_curr+tab[index//len(tab)][index%len(tab)],suma_zad,(index//len(tab))+1,tablica):
             return True
-        wybrane.pop()
+        tablica[index] = False
     return False
 
 def func(tab,suma):
-    return reku(tab,0,suma,[],0)
+    tablica = [False for _ in range(len(tab)**2)]
+    return reku(tab,0,suma,0,tablica)
 
 tab = [[0,0,0,1],[1,0,0,0],[0,1,0,0],[0,0,1,0]]
+
 print(func(tab,4))
