ba6672dbb46da44a040de026d27db9fad23cc791
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-12-19 12:13:28 +0100

    create cw16

--- cwiczenia2020/Cwiczenia 7/cw15.py
+++ cwiczenia2020/Cwiczenia 7/cw15.py
@@ -1,4 +1,5 @@
 null = None
+
 class Node:
     def __init__(self, value=null, next=null):
         self.val = value
@@ -9,48 +10,52 @@ class Node:
 
 
 def wypisz(first):
-    if first==null:
+    if first == null:
         print("List is empty!")
     while first != null:
         print(first, end='')
         first = first.next
     print()
-#!15. Proszę napisać funkcję, która otrzymując jako parametr wskazujący na
-#!początek listy jednokierunkowej, usuwa z niej wszystkie elementy, w których
-#!wartość klucza w zapisie trójkowym ma większą ilość jedynek niż dwójek.
 
+
+# !15. Proszę napisać funkcję, która otrzymując jako parametr wskazujący na
+# !początek listy jednokierunkowej, usuwa z niej wszystkie elementy, w których
+# !wartość klucza w zapisie trójkowym ma większą ilość jedynek niż dwójek.
 def delete(cp, cp2):
     cp.next = cp2.next
     del cp2
     return cp
 
+
 def check_3(num):
-    i_1=0
-    i_2=0
-    while num!=0:
-        a = num%3
-        if a ==1:
-            i_1+=1
-        if a==2:
-            i_2+=1
-        num//=3
-    return i_1>i_2
+    i_1 = 0
+    i_2 = 0
+    while num != 0:
+        a = num % 3
+        if a == 1:
+            i_1 += 1
+        if a == 2:
+            i_2 += 1
+        num //= 3
+    return i_1 > i_2
+
 
 def check(first):
-    while first!= null and check_3(first.val):
+    while first != null and check_3(first.val):
         first = first.next
-    if first==null:
+    if first == null:
         return null
-    cp,cp2 = first,first.next
-    while cp2!=null:
+    cp, cp2 = first, first.next
+    while cp2 != null:
         if check_3(cp2.val):
-            delete(cp,cp2)
-        cp,cp2=cp2,cp2.next
+            delete(cp, cp2)
+        cp, cp2 = cp2, cp2.next
     return first
 
-first=null
+
+first = null
 for i in range(20):
-    a = Node(16,first)
+    a = Node(16, first)
     first = a
 wypisz(first)
 wypisz(check(first))
