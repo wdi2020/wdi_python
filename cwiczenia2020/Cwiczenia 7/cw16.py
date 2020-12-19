bd9e0f12b6febe567f24ffc40fe056385099ebd2
hdbdg8 <lukaszpawlak162.lp@gmail.com>
2020-12-19 12:25:03 +0100

    create cw17

--- cwiczenia2020/Cwiczenia 7/cw16.py
+++ cwiczenia2020/Cwiczenia 7/cw16.py
@@ -1,4 +1,5 @@
 null = None
+
 class Node:
     def __init__(self, value=null, next=null):
         self.val = value
@@ -16,42 +17,44 @@ def wypisz(first):
         first = first.next
     print()
 
-#16. Proszę napisać funkcję, która otrzymując jako parametr wskazujący na
-#początek listy jednokierunkowej, przenosi na początek listy te z nich,
-#które mają parzystą ilość piątek w zapisie ósemkowym.
+
+# 16. Proszę napisać funkcję, która otrzymując jako parametr wskazujący na
+# początek listy jednokierunkowej, przenosi na początek listy te z nich,
+# które mają parzystą ilość piątek w zapisie ósemkowym.
 
 def ile_5(num):
-    ilosc=0
-    while num!=0:
-        if num%8==5:
-            ilosc+=1
-        num//=8
+    ilosc = 0
+    while num != 0:
+        if num % 8 == 5:
+            ilosc += 1
+        num //= 8
     return ilosc
 
-def move(first,cp,cp2):
+
+def move(first, cp, cp2):
     cp.next = cp2.next
     cp2.next = first
     return cp2
 
+
 def check(first):
-    if first==null:
+    if first == null:
         return null
     if first.next == null:
         return first
-    cp,cp2 = first,first.next
-    while cp2!=null:
-        if ile_5(cp2.val)%2==0:
-            first = move(first,cp,cp2)
+    cp, cp2 = first, first.next
+    while cp2 != null:
+        if ile_5(cp2.val) % 2 == 0:
+            first = move(first, cp, cp2)
             cp2 = cp.next
         else:
-            cp,cp2=cp2,cp2.next
+            cp, cp2 = cp2, cp2.next
     return first
 
-first=Node(1)
+
+first = Node(1)
 for i in range(10):
-    a = Node(5,first)
+    a = Node(5, first)
     first = a
 wypisz(first)
 wypisz(check(first))
-
-
